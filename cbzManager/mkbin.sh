f74f9f78969afce777002c9c273e2a008e82948c
Ollivier Civiol <ollivier@civiol.eu>
2019-08-23 17:37:41 +0200

    Release build

--- cbzManager/mkbin.sh
+++ cbzManager/mkbin.sh
@@ -1,4 +1,5 @@
-
+cd /Users/ollivierciviol/Dev/cbzManager/cbzManager 
+/Users/ollivierciviol/fpcupdeluxe/fpc/bin/x86_64-darwin/fpc.sh -B -MObjFPC -Scghi -CX -O3 -XX -k-framework -kCocoa -l -vewnhibq -Fi/Users/ollivierciviol/Dev/cbzManager/cbzManager/lib/x86_64-darwin -Fu/Users/ollivierciviol/Dev/cbzManager/Utils -Fu/Users/ollivierciviol/Dev/cbzManager/Utils/zipfile -Fu/Users/ollivierciviol/fpcupdeluxe/lazarus/lcl/units/x86_64-darwin/cocoa -Fu/Users/ollivierciviol/fpcupdeluxe/lazarus/lcl/units/x86_64-darwin -Fu/Users/ollivierciviol/fpcupdeluxe/lazarus/components/lazutils/lib/x86_64-darwin -Fu/Users/ollivierciviol/fpcupdeluxe/lazarus/packager/units/x86_64-darwin -Fu/Users/ollivierciviol/Dev/cbzManager/cbzManager/ -FU/Users/ollivierciviol/Dev/cbzManager/cbzManager/lib/x86_64-darwin/ -FE/Users/ollivierciviol/Dev/cbzManager/cbzManager/ -o/Users/ollivierciviol/Dev/cbzManager/cbzManager/cbzManagerOsx -dLCL -dLCLcocoa -dUseCThreads cbzManagerOsx.lpr
 cp -r cbzManagerOsx.app ../precompiled\ binairies/Mac\ OsX/ 
 rm ../precompiled\ binairies/Mac\ OsX/cbzManagerOsx.app/Contents/MacOS/cbzManagerOsx 
 cp cbzManagerOsx ../precompiled\ binairies/Mac\ OsX/cbzManagerOsx.app/Contents/MacOS/

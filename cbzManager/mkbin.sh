72e6356aaefe00efa1c5d46a1f73fedd1fc5ffef
Ollivier Civiol <ollivier@civiol.eu>
2019-08-23 10:45:46 +0200

    Precompiled binairies

--- cbzManager/mkbin.sh
+++ cbzManager/mkbin.sh
@@ -2,4 +2,8 @@
 cp -r cbzManagerOsx.app ../precompiled\ binairies/Mac\ OsX/ 
 rm ../precompiled\ binairies/Mac\ OsX/cbzManagerOsx.app/Contents/MacOS/cbzManagerOsx 
 cp cbzManagerOsx ../precompiled\ binairies/Mac\ OsX/cbzManagerOsx.app/Contents/MacOS/
+cd ../precompiled\ binairies/Mac\ OsX/
+rm cbzManagerOsx.zip
+zip -r cbzManagerOsx.zip cbzManagerOsx.app
+cp -r cbzManagerOsx.app /Applications/ 
 

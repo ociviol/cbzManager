2a8f098a763ab81de4ca0dc34547027055514ba5
Ollivier Civiol <ollivier@civiol.eu>
2019-08-23 15:09:39 +0200

    Removed files

--- precompiled binairies/Linux/installlinux.sh
+++
@@ -1,7 +0,0 @@
-chmod +x cbzManager
-chmod +x cbzManager.desktop
-cp cbzManager /usr/local/bin
-cp cbzManager.png /usr/local/bin
-ln -s /usr/local/bin/cbzManager /usr/bin/cbzManager
-cp cbzManager.desktop /usr/share/applications/ 
-

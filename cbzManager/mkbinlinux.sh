6b986be83f75b6d935c1dcaf3e5c39219d344d6e
Ollivier Civiol <ollivier@civiol.eu>
2019-08-23 13:37:37 +0200

    Added icon to desktop icon

--- cbzManager/mkbinlinux.sh
+++ cbzManager/mkbinlinux.sh
@@ -1,4 +1,7 @@
 cp cbzManager ../precompiled\ binairies/Linux/
 cp /usr/share/applications/cbzManager.desktop ../precompiled\ binairies/Linux/
 sudo cp cbzManager /usr/local/bin
-
+cd /home/matugenos/Dev/cbzManager/precompiled\ binairies/Linux/
+cp /home/matugenos/Dev/cbzManager/cbzManager/cbzManagerOsx.iconset/icon_128x128.png cbzManager.png
+rm cbzManager.zip
+zip cbzManager.zip *

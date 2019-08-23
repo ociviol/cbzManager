142f6beb15528a98175cc84e99108b5a074120b1
Ollivier Civiol <ollivier@civiol.eu>
2019-08-23 10:23:43 +0200

    add install script

---
+++ cbzManager/mkbinlinux.sh
@@ -0,0 +1,4 @@
+cp cbzManager ../precompiled\ binairies/Linux/
+cp /usr/share/applications/cbzManager.desktop ../precompiled\ binairies/Linux/
+sudo cp cbzManager /usr/local/bin
+
---
+++ precompiled binairies/Linux/installlinux.sh
@@ -0,0 +1,5 @@
+cp cbzManager /usr/local/bin
+ln -s /usr/local/bin/cbzManager /usr/bin/cbzManager
+cp cbzManager.desktop /usr/share/applications/ 
+chmod +x /usr/share/applications/cbzManager.desktop
+

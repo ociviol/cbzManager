947e89bc2fa8cc105911d97130a30c8e587dfb6d
Ollivier Civiol <ollivier@civiol.eu>
2019-09-10 17:13:51 +0200

    work on windows version

---
+++ cbzManager/mkbinwin.bat
@@ -0,0 +1,4 @@
+
+C:\lazarus\fpc\3.0.4\bin\x86_64-win64\fpc.exe -B -Twin64 -MObjFPC -Scghi -CX -O3 -XX -WG -l -vewnhibq -vm5076,5044,4066,4056,4055 -Fi.\lib\x86_64-win64 -FuC:\Users\ociviol\Documents\GitHub\cbzManager\Utils -FuC:\Users\ociviol\Documents\GitHub\cbzManager\common -FuC:\lazarus\components\turbopower_ipro\units\x86_64-win64\win32 -FuC:\lazarus\components\printers\lib\x86_64-win64\win32 -FuC:\lazarus\components\cairocanvas\lib\x86_64-win64\win32 -FuC:\lazarus\lcl\units\x86_64-win64\win32 -FuC:\lazarus\lcl\units\x86_64-win64 -FuC:\lazarus\components\lazutils\lib\x86_64-win64 -FuC:\lazarus\packager\units\x86_64-win64 -Fu.\ -FU.\lib\x86_64-win64\ -FE.\ -o"..\precompiled binairies\Windows\cbzManager.exe" -dLCL -dLCLwin32 -dUseCThreads -dRELEASE cbzManagerWin.lpr
+copy ..\Bin-Win\*.* "..\precompiled binairies\Windows"
+

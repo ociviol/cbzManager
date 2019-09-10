cf62955e9a3c99261386107c0ca2903131022bd0
Ollivier Civiol <ollivier@civiol.eu>
2019-09-10 17:23:58 +0200

    Windows binary (work in progress)

--- cbzManager/mkbinwin.bat
+++ cbzManager/mkbinwin.bat
@@ -1,4 +1,8 @@
 
 C:\lazarus\fpc\3.0.4\bin\x86_64-win64\fpc.exe -B -Twin64 -MObjFPC -Scghi -CX -O3 -XX -WG -l -vewnhibq -vm5076,5044,4066,4056,4055 -Fi.\lib\x86_64-win64 -FuC:\Users\ociviol\Documents\GitHub\cbzManager\Utils -FuC:\Users\ociviol\Documents\GitHub\cbzManager\common -FuC:\lazarus\components\turbopower_ipro\units\x86_64-win64\win32 -FuC:\lazarus\components\printers\lib\x86_64-win64\win32 -FuC:\lazarus\components\cairocanvas\lib\x86_64-win64\win32 -FuC:\lazarus\lcl\units\x86_64-win64\win32 -FuC:\lazarus\lcl\units\x86_64-win64 -FuC:\lazarus\components\lazutils\lib\x86_64-win64 -FuC:\lazarus\packager\units\x86_64-win64 -Fu.\ -FU.\lib\x86_64-win64\ -FE.\ -o"..\precompiled binairies\Windows\cbzManager.exe" -dLCL -dLCLwin32 -dUseCThreads -dRELEASE cbzManagerWin.lpr
-copy ..\Bin-Win\*.* "..\precompiled binairies\Windows"
+copy .\Bin-Win\*.* "..\precompiled binairies\Windows"
+cd "..\precompiled binairies\Windows"
+del cbzManager.7z
+7z a -sdel cbzManager.7z *.exe *.dll
+del *.exe
 

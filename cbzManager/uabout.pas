d792d5188a20b5ba910cd5377e80c60ca929af53
Ollivier Civiol <ollivier@civiol.eu>
2019-09-26 06:34:09 +0200

    added info in about

--- cbzManager/uabout.pas
+++ cbzManager/uabout.pas
@@ -26,23 +26,29 @@ implementation
 
 uses
   Webp,
+  StrUtils,
   Utils.SoftwareVersion;
 
 { TfrmAbout }
 
 procedure TfrmAbout.FormCreate(Sender: TObject);
+var
+  dv, ev : integer;
 begin
+  dv := DoWebPGetDecoderVersion;
+  ev := DoWebPGetEncoderVersion;
+
   Label1.Caption := GetFileVersionInternalName + ' ' +
                     GetFileVersion + ' © ' + GetFileVersionCopyright;
   if not InternalcWebpAvail then
     Listbox1.Items.Add('Internal Webp Encoder unavailable.')
   else
-    Listbox1.Items.Add('Webp Encoder Version : ' + IntToStr(DoWebPGetEncoderVersion));
+    Listbox1.Items.Add('Webp Encoder Version : ' + ifthen(ev > 0, IntToStr(ev), 'Unavailable'));
 
   if not InternaldWebpAvail then
     Listbox1.Items.Add('Internal Webp Decoder unavailable.')
   else
-    Listbox1.Items.Add('Webp Decoder Version : ' + IntToStr(DoWebPGetDecoderVersion));
+    Listbox1.Items.Add('Webp Decoder Version : ' + ifthen(dv > 0, IntToStr(dv), 'Unavailable'));
 end;
 
 end.

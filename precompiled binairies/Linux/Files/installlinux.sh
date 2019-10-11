fcdd4b9b6f346a0d3d802303de94e549bf6aa454
Ollivier Civiol <ollivier@civiol.eu>
2019-10-11 09:00:26 +0200

    Linux Files

---
+++ precompiled binairies/Linux/Files/installlinux.sh
@@ -0,0 +1,7 @@
+chmod +x cbzManager
+chmod +x cbzManager.desktop
+cp cbzManager /usr/local/bin
+cp cbzManager.png /usr/local/bin
+ln -s /usr/local/bin/cbzManager /usr/bin/cbzManager
+cp cbzManager.desktop /usr/share/applications/ 
+

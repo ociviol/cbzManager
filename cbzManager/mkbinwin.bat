11e176dba208357e389f0fb31dd2785a1f3a6bb6
Ollivier Civiol <ollivierciviol.eu>
2023-03-13 15:51:59 +0100

    Updated windows version

--- cbzManager/mkbinwin.bat
+++ cbzManager/mkbinwin.bat
@@ -1,14 +1,13 @@
-C:\fpcupdeluxe\fpc\bin\x86_64-win64\fpc.exe  -Twin64 -MObjFPC -Scaghi -CiroR -gw2 -godwarfsets -gl -gh -Xg -gt -WG -l -vabq -vm5076,5044,4066,4056,4055 -Filib\x86_64-win64 -Fu..\..\Utils -Fu..\..\XmlReader\XmlDoc -Fu..\..\Synapse\source\lib -FuC:\fpcupdeluxe\lazarus\components\virtualtreeview\lib\x86_64-win64-win32 -FuC:\fpcupdeluxe\lazarus\components\turbopower_ipro\units\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\components\printers\lib\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\components\ideintf\units\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\components\lclextensions\lib\x86_64-win64-win32 -FuC:\fpcupdeluxe\lazarus\components\lazcontrols\lib\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\components\cairocanvas\lib\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\lcl\units\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\lcl\units\x86_64-win64 -FuC:\fpcupdeluxe\lazarus\components\lazutils\lib\x86_64-win64 -FuC:\fpcupdeluxe\lazarus\packager\units\x86_64-win64 -Fu. -FUlib\x86_64-win64 -FE. -ocbzManager.exe -dLCL -dLCLwin32 -dUseCThreads -dRELEASE cbzManagerWin.lpr
+C:\fpcupdeluxe\fpc\bin\x86_64-win64\fpc.exe -B -Twin64 -MObjFPC -Scghi -CX -O2 -XX -WG -l -vewnhibq -vm5076,5044,4066,4056,4055 -Filib\x86_64-win64 -Fu..\..\Utils -Fu..\..\XmlReader\XmlDoc -FuC:\fpcupdeluxe\lazarus\components\turbopower_ipro\units\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\components\printers\lib\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\components\cairocanvas\lib\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\lcl\units\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\lcl\units\x86_64-win64 -FuC:\fpcupdeluxe\lazarus\components\freetype\lib\x86_64-win64 -FuC:\fpcupdeluxe\lazarus\components\lazutils\lib\x86_64-win64 -FuC:\fpcupdeluxe\lazarus\packager\units\x86_64-win64 -Fu. -FUlib\x86_64-win64 -FE. -o.\Bin-Win\cbzManager.exe -dLCL -dLCLwin32 -dUseCThreads -dRELEASE cbzManagerWin.lpr
 
 copy .\Bin-Win\*.* "..\precompiled binairies\Windows"
 cd "..\precompiled binairies\Windows"
 
 rem pause
-copy cbzManager.exe C:\Users\ollivierciviol\OneDrive\Documents\cbzManager\
 
 del cbzManagerWin.zip
 copy cbzManager.exe %USERPROFILE%\OneDrive\Documents\cbzManager\
-7z a -tzip -sdel cbzManagerWin.zip *.exe *.dll
+7z a -tzip -sdel cbzManagerWin.zip *.exe *.dll *.txt
 
-del *.exe *.dll
+del *.exe *.dll *.txt
 pause

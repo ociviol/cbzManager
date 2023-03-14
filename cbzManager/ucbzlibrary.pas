b12e200867ed174a66e0a6c1c7a5753fedcb225a
Ollivier Civiol <ollivier@civiol.eu>
2023-03-14 12:45:34 +0100

    Work on library

--- cbzManager/ucbzlibrary.pas
+++ cbzManager/ucbzlibrary.pas
@@ -563,9 +563,6 @@ begin
   FVisibleList.OnChanging := @VisibleListChanged;
   //FVisibleList.Sorted:=True;
 
-  dgLibrary.DefaultColWidth:=Fconfig.DefaultColWidth;
-  dgLibrary.DefaultRowHeight:=Fconfig.DefaultRowHeight;
-
   s := IncludeTrailingPathDelimiter(FConfig.SyncPath) + 'cbzReadingList.json';
   if FileExists(s) then
   begin

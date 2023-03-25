b26b9e271c25af45c4f9e106ca5eeb57d58c533f
Ollivier Civiol <ollivier@civiol.eu>
2023-03-25 13:56:30 +0100

    Added OsX Library project

--- cbzManager/mkbin.sh
+++ cbzManager/mkbin.sh
@@ -1,15 +1,29 @@
 cd ~/Dev/cbzManager/cbzManager 
 /Users/ollivierciviol/fpcupdeluxe/fpc/bin/x86_64-darwin/fpc.sh -B -MObjFPC -Scghi -CX -O3 -XX -k-framework -kCocoa -l -vewnhibq -Filib/x86_64-darwin -Fu../../Utils -Fu../../XmlReader/XmlDoc -Fu../../../fpcupdeluxe/lazarus/lcl/units/x86_64-darwin/cocoa -Fu../../../fpcupdeluxe/lazarus/lcl/units/x86_64-darwin -Fu../../../fpcupdeluxe/lazarus/components/freetype/lib/x86_64-darwin -Fu../../../fpcupdeluxe/lazarus/components/lazutils/lib/x86_64-darwin -Fu../../../fpcupdeluxe/lazarus/packager/units/x86_64-darwin -Fu. -FUlib/x86_64-darwin -FE. -ocbzManagerOsx -dLCL -dLCLcocoa -dUseCThreads cbzManagerOsx.lpr
+
+cd ~/Dev/cbzManager/cbzManager/Library 
+/Users/ollivierciviol/fpcupdeluxe/fpc/bin/x86_64-darwin/fpc.sh  -MObjFPC -Scghi -CX -O3 -XX -k-framework -kCocoa -l -vewnhibq -Filib/x86_64-darwin -Fu.. -Fu../../../../fpcupdeluxe/lazarus/lcl/units/x86_64-darwin/cocoa -Fu../../../../fpcupdeluxe/lazarus/lcl/units/x86_64-darwin -Fu../../../../fpcupdeluxe/lazarus/components/freetype/lib/x86_64-darwin -Fu../../../../fpcupdeluxe/lazarus/components/lazutils/lib/x86_64-darwin -Fu../../../../fpcupdeluxe/lazarus/packager/units/x86_64-darwin -Fu. -FUlib/x86_64-darwin -FE. -ocbzLibraryOsx -dLCL -dLCLcocoa -dUseCThreads cbzLibraryOsx.lpr
+
+
 status=$?
 if test $status -eq 0 
 then
-	cp -r cbzManagerOsx.app ../precompiled\ binairies/Mac\ OsX/ 
-	rm ../precompiled\ binairies/Mac\ OsX/cbzManagerOsx.app/Contents/MacOS/cbzManagerOsx 
-	cp cbzManagerOsx ../precompiled\ binairies/Mac\ OsX/cbzManagerOsx.app/Contents/MacOS/
-	cd ../precompiled\ binairies/Mac\ OsX/
+	cp -r ~/Dev/cbzManager/cbzManager/Library/cbzLibraryOsx.app ~/Dev/cbzManager//precompiled\ binairies/Mac\ OsX/ 
+	cp -r ~/Dev/cbzManager/cbzManager/cbzManagerOsx.app ~/Dev/cbzManager//precompiled\ binairies/Mac\ OsX/ 
+
+	rm ~/Dev/cbzManager/precompiled\ binairies/Mac\ OsX/cbzManagerOsx.app/Contents/MacOS/cbzManagerOsx 
+	cp ~/Dev/cbzManager/cbzManager/cbzManagerOsx ~/Dev/cbzManager/precompiled\ binairies/Mac\ OsX/cbzManagerOsx.app/Contents/MacOS/
+
+	rm ~/Dev/cbzManager/precompiled\ binairies/Mac\ OsX/cbzLibraryOsx.app/Contents/MacOS/cbzLibraryOsx 
+	cp ~/Dev/cbzManager/cbzManager/Library/cbzLibraryOsx ~/Dev/cbzManager/precompiled\ binairies/Mac\ OsX/cbzLibraryOsx.app/Contents/MacOS/
+
+	cp -r ~/Dev/cbzManager/precompiled\ binairies/Mac\ OsX/cbzManagerOsx.app /Applications/ 
+	cp -r ~/Dev/cbzManager/precompiled\ binairies/Mac\ OsX/cbzLibraryOsx.app /Applications/
+
+	cd ~/Dev/cbzManager/precompiled\ binairies/Mac\ OsX/
 	rm cbzManagerOsx.zip
-	zip -r cbzManagerOsx.zip cbzManagerOsx.app
-	cp -r cbzManagerOsx.app /Applications/ 
+	zip -r cbzManagerOsx.zip cp -r ~/Dev/cbzManager/cbzManager/cbzManagerOsx.app cp -r ~/Dev/cbzManager/cbzManager/Library/cbzLibraryOsx.app
+ 
 else
 	echo 'Compile failed'
 fi

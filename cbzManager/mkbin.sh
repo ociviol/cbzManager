384bcd79f8d371e2697eceffb0bf10c1f8b9e316
Ollivier Civiol <ollivier@civiol.eu>
2023-07-24 14:33:17 +0200

    Added missing define for library build

--- cbzManager/mkbin.sh
+++ cbzManager/mkbin.sh
@@ -3,7 +3,7 @@ cd ~/Dev/cbzManager/cbzManager
 
 cd ~/Dev/cbzManager/cbzManager/Library 
 
-/Users/ollivierciviol/fpcupdeluxe/fpc/bin/x86_64-darwin/fpc.sh  -MObjFPC -Scghi -CX -O3 -XX -k-framework -kCocoa -l -vewnhibq -Filib/x86_64-darwin -Fu../../../Utils -Fu../../../XmlReader/XmlDoc -Fu.. -Fu../../../../fpcupdeluxe/lazarus/lcl/units/x86_64-darwin/cocoa -Fu../../../../fpcupdeluxe/lazarus/lcl/units/x86_64-darwin -Fu../../../../fpcupdeluxe/lazarus/components/freetype/lib/x86_64-darwin -Fu../../../../fpcupdeluxe/lazarus/components/lazutils/lib/x86_64-darwin -Fu../../../../fpcupdeluxe/lazarus/packager/units/x86_64-darwin -Fu. -FUlib/x86_64-darwin -FE. -ocbzLibraryOsx -dLCL -dLCLcocoa -dUseCThreads cbzLibraryOsx.lpr
+/Users/ollivierciviol/fpcupdeluxe/fpc/bin/x86_64-darwin/fpc.sh  -MObjFPC -Scghi -CX -O3 -XX -k-framework -kCocoa -l -vewnhibq -Filib/x86_64-darwin -Fu../../../Utils -Fu../../../XmlReader/XmlDoc -Fu.. -Fu../../../../fpcupdeluxe/lazarus/lcl/units/x86_64-darwin/cocoa -Fu../../../../fpcupdeluxe/lazarus/lcl/units/x86_64-darwin -Fu../../../../fpcupdeluxe/lazarus/components/freetype/lib/x86_64-darwin -Fu../../../../fpcupdeluxe/lazarus/components/lazutils/lib/x86_64-darwin -Fu../../../../fpcupdeluxe/lazarus/packager/units/x86_64-darwin -Fu. -FUlib/x86_64-darwin -FE. -ocbzLibraryOsx -dLibrary -dLCL -dLCLcocoa -dUseCThreads cbzLibraryOsx.lpr
 
 
 status=$?
--- cbzManager/mkbinwin.bat
+++ cbzManager/mkbinwin.bat
@@ -2,7 +2,7 @@ C:\fpcupdeluxe\fpc\bin\x86_64-win64\fpc.exe -B -Twin64 -MObjFPC -Scghi -CX -O2 -
 
 cd Library
 
-C:\fpcupdeluxe\fpc\bin\x86_64-win64\fpc.exe -B -Twin64 -MObjFPC -Scghi -CX -O2 -XX -WG -l -vewnhibq -vm5076,5044,4066,4056,4055 -Filib\x86_64-win64 -Fu..\..\..\Utils -Fu..\..\..\XmlReader\XmlDoc -FuC:\fpcupdeluxe\lazarus\components\turbopower_ipro\units\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\components\printers\lib\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\components\cairocanvas\lib\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\lcl\units\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\lcl\units\x86_64-win64 -FuC:\fpcupdeluxe\lazarus\components\freetype\lib\x86_64-win64 -FuC:\fpcupdeluxe\lazarus\components\lazutils\lib\x86_64-win64 -FuC:\fpcupdeluxe\lazarus\packager\units\x86_64-win64 -Fu. -FUlib\x86_64-win64 -FE. -o..\Bin-Win\cbzLibrary.exe -dLCL -dLCLwin32 -dUseCThreads -dRELEASE cbzLibraryWin.lpr
+C:\fpcupdeluxe\fpc\bin\x86_64-win64\fpc.exe -B -Twin64 -MObjFPC -Scghi -CX -O2 -XX -WG -l -vewnhibq -vm5076,5044,4066,4056,4055 -Filib\x86_64-win64 -Fu..\..\..\Utils -Fu..\..\..\XmlReader\XmlDoc -FuC:\fpcupdeluxe\lazarus\components\turbopower_ipro\units\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\components\printers\lib\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\components\cairocanvas\lib\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\lcl\units\x86_64-win64\win32 -FuC:\fpcupdeluxe\lazarus\lcl\units\x86_64-win64 -FuC:\fpcupdeluxe\lazarus\components\freetype\lib\x86_64-win64 -FuC:\fpcupdeluxe\lazarus\components\lazutils\lib\x86_64-win64 -FuC:\fpcupdeluxe\lazarus\packager\units\x86_64-win64 -Fu. -FUlib\x86_64-win64 -FE. -o..\Bin-Win\cbzLibrary.exe -dLibrary -dLCL -dLCLwin32 -dUseCThreads -dRELEASE cbzLibraryWin.lpr
 
 cd ..
 

ddc604c365c6210cecae138770c97f8150910c78
Ollivier Civiol <ollivier@civiol.com>
2024-06-17 20:23:30 +0200

    fix output name in projects

--- cbzManager/mkbin-arm64.sh
+++ cbzManager/mkbin-arm64.sh
@@ -1,8 +1,8 @@
 cd ~/Dev/cbzManager/cbzManager 
-/Users/ollivierciviol/fpcupdeluxe/fpc/bin/aarch64-darwin/fpc.sh -B -MObjFPC -Scghi -CX -O3 -XX -k-framework -kCocoa -l -vewnhibq -Filib/aarch64-darwin -Fu../../Utils -Fu../../XmlReader/XmlDoc -Fu../../../fpcupdeluxe/lazarus/lcl/units/aarch64-darwin/cocoa -Fu../../../fpcupdeluxe/lazarus/lcl/units/aarch64-darwin -Fu../../../fpcupdeluxe/lazarus/components/freetype/lib/aarch64-darwin -Fu../../../fpcupdeluxe/lazarus/components/lazutils/lib/aarch64-darwin -Fu../../../fpcupdeluxe/lazarus/packager/units/aarch64-darwin -Fu. -FUlib/aarch64-darwin -FE. -ocbzmanagerosx -dLCL -dLCLcocoa -dUseCThreads -darm cbzmanagerosxarm.lpr
+/Users/ollivierciviol/fpcupdeluxe/fpc/bin/aarch64-darwin/fpc.sh -B -MObjFPC -Scghi -CX -O3 -XX -k-framework -kCocoa -l -vewnhibq -Filib/aarch64-darwin -Fu../../Utils -Fu../../XmlReader/XmlDoc -Fu../../../fpcupdeluxe/lazarus/lcl/units/aarch64-darwin/cocoa -Fu../../../fpcupdeluxe/lazarus/lcl/units/aarch64-darwin -Fu../../../fpcupdeluxe/lazarus/components/freetype/lib/aarch64-darwin -Fu../../../fpcupdeluxe/lazarus/components/lazutils/lib/aarch64-darwin -Fu../../../fpcupdeluxe/lazarus/packager/units/aarch64-darwin -Fu. -FUlib/aarch64-darwin -FE. -ocbzManagerOsx -dLCL -dLCLcocoa -dUseCThreads -darm cbzmanagerosxarm.lpr
 
 cd ~/Dev/cbzManager/cbzManager/Library 
-/Users/ollivierciviol/fpcupdeluxe/fpc/bin/aarch64-darwin/fpc.sh -B -MObjFPC -Scghi -CX -O3 -XX -k-framework -kCocoa -l -vewnhibq -Filib/aarch64-darwin -Fu../../../Utils -Fu../../../XmlReader/XmlDoc -Fu.. -Fu../../../../fpcupdeluxe/lazarus/components/sqlite/lib/aarch64-darwin/cocoa -Fu../../../../fpcupdeluxe/lazarus/components/ideintf/units/aarch64-darwin/cocoa -Fu../../../../fpcupdeluxe/lazarus/components/lazcontrols/lib/aarch64-darwin/cocoa -Fu../../../../fpcupdeluxe/lazarus/lcl/units/aarch64-darwin/cocoa -Fu../../../../fpcupdeluxe/lazarus/lcl/units/aarch64-darwin -Fu../../../../fpcupdeluxe/lazarus/components/freetype/lib/aarch64-darwin -Fu../../../../fpcupdeluxe/lazarus/components/buildintf/units/aarch64-darwin -Fu../../../../fpcupdeluxe/lazarus/components/lazutils/lib/aarch64-darwin -Fu../../../../fpcupdeluxe/lazarus/packager/units/aarch64-darwin -Fu. -FUlib/aarch64-darwin -FE. -ocbzlibraryosx -dLCL -dLCLcocoa -darm -dUseCThreads -dLibrary cbzlibraryosxarm.lpr
+/Users/ollivierciviol/fpcupdeluxe/fpc/bin/aarch64-darwin/fpc.sh -B -MObjFPC -Scghi -CX -O3 -XX -k-framework -kCocoa -l -vewnhibq -Filib/aarch64-darwin -Fu../../../Utils -Fu../../../XmlReader/XmlDoc -Fu.. -Fu../../../../fpcupdeluxe/lazarus/components/sqlite/lib/aarch64-darwin/cocoa -Fu../../../../fpcupdeluxe/lazarus/components/ideintf/units/aarch64-darwin/cocoa -Fu../../../../fpcupdeluxe/lazarus/components/lazcontrols/lib/aarch64-darwin/cocoa -Fu../../../../fpcupdeluxe/lazarus/lcl/units/aarch64-darwin/cocoa -Fu../../../../fpcupdeluxe/lazarus/lcl/units/aarch64-darwin -Fu../../../../fpcupdeluxe/lazarus/components/freetype/lib/aarch64-darwin -Fu../../../../fpcupdeluxe/lazarus/components/buildintf/units/aarch64-darwin -Fu../../../../fpcupdeluxe/lazarus/components/lazutils/lib/aarch64-darwin -Fu../../../../fpcupdeluxe/lazarus/packager/units/aarch64-darwin -Fu. -FUlib/aarch64-darwin -FE. -ocbzLibraryOsx -dLCL -dLCLcocoa -darm -dUseCThreads -dLibrary cbzlibraryosxarm.lpr
 
 status=$?
 if test $status -eq 0 
@@ -15,9 +15,6 @@ then
 
 	rm ~/Dev/cbzManager/precompiled\ binairies/Mac\ OsX/arm64/cbzLibraryOsx.app/Contents/MacOS/cbzLibraryOsx 
 	cp ~/Dev/cbzManager/cbzManager/Library/cbzLibraryOsx ~/Dev/cbzManager/precompiled\ binairies/Mac\ OsX/arm64/cbzLibraryOsx.app/Contents/MacOS/
-
-	cp -r ~/Dev/cbzManager/precompiled\ binairies/Mac\ OsX/arm64/cbzManagerOsx.app /Applications/ 
-	cp -r ~/Dev/cbzManager/precompiled\ binairies/Mac\ OsX/arm64/cbzLibraryOsx.app /Applications/
 
 	cd ~/Dev/cbzManager/precompiled\ binairies/Mac\ OsX/arm64/
 	rm cbzManagerOsx-arm64.zip

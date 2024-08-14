35f1932297444ef1aed893a2e63b4935590b74ae
Ollivier Civiol <ollivier@civiol.eu>
2024-08-14 15:55:22 +0200

    Update mkbinlinux.sh

--- cbzManager/mkbinlinux.sh
+++ cbzManager/mkbinlinux.sh
@@ -1,9 +1,8 @@
 cd ~/Dev/cbzManager/cbzManager
+/home/mat/fpcupdeluxe/lazarus/lazbuild -B cbzManager.lpi
 
-'/home/mat/fpcupdeluxe/fpc/bin/x86_64-linux/fpc.sh' -B -Tlinux -MObjFPC -Scghi -CX -Cg -O3 -XX -l -vewnhibq -vm5076,5044,4066,4056,4055 -Filib/x86_64-linux -Fu../../Utils -Fu../../XmlReader/XmlDoc -Fu../../../fpcupdeluxe/lazarus/components/turbopower_ipro/units/x86_64-linux/gtk2 -Fu../../../fpcupdeluxe/lazarus/components/printers/lib/x86_64-linux/gtk2 -Fu../../../fpcupdeluxe/lazarus/components/cairocanvas/lib/x86_64-linux/gtk2 -Fu../../../fpcupdeluxe/lazarus/lcl/units/x86_64-linux/gtk2 -Fu../../../fpcupdeluxe/lazarus/lcl/units/x86_64-linux -Fu../../../fpcupdeluxe/lazarus/components/freetype/lib/x86_64-linux -Fu../../../fpcupdeluxe/lazarus/components/lazutils/lib/x86_64-linux -Fu../../../fpcupdeluxe/lazarus/packager/units/x86_64-linux -Fu. -FUlib/x86_64-linux -FE. -ocbzManager -dLCL -dLCLgtk2 -dUseCThreads -dRELEASE cbzManager.lpr
-
-cd ~/Dev/cbzManager/cbzManager/Library
-'/home/mat/fpcupdeluxe/fpc/bin/x86_64-linux/fpc.sh'  -B -MObjFPC -Scghi -CX -Cg -O3 -XX -l -vewnhibq -Filib/x86_64-linux -Fu.. -Fu../../../../fpcupdeluxe/lazarus/lcl/units/x86_64-linux/gtk2 -Fu../../../../fpcupdeluxe/lazarus/lcl/units/x86_64-linux -Fu../../../../fpcupdeluxe/lazarus/components/freetype/lib/x86_64-linux -Fu../../../../fpcupdeluxe/lazarus/components/lazutils/lib/x86_64-linux -Fu../../../../fpcupdeluxe/lazarus/packager/units/x86_64-linux -Fu. -FUlib/x86_64-linux -FE. -ocbzLibrary -dLibrary -dLCL -dLCLgtk2 -dUseCThreads -dRELEASE cbzLibraryLinux.lpr
+cd ~/Dev/cbzManager/cbzManager/Library 
+/home/mat/fpcupdeluxe/lazbuild -B cbzLibraryLinux.lpi
 
 cp ~/Dev/cbzManager/cbzManager/cbzManager ~/Dev/cbzManager/precompiled\ binairies/Linux/Files
 cp ~/Dev/cbzManager/cbzManager/Library/cbzLibrary ~/Dev/cbzManager/precompiled\ binairies/Linux/Files
